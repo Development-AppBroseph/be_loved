31e4f09894f8ac2590015e7f6423ac2d7d208bb4
kkkivannn <ikiselev288@gmail.com>
2023-06-07 20:24:22 +0300

    Fixed bugs with update

--- lib/my_app/data/datasource.dart
+++ lib/my_app/data/datasource.dart
@@ -1,5 +1,6 @@
 import 'package:be_loved/core/services/network/config.dart';
 import 'package:be_loved/my_app/models/image_model.dart';
+import 'package:be_loved/my_app/models/version_model.dart';
 import 'package:dio/dio.dart';
 import 'package:intl/intl.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -34,6 +35,17 @@ class Datasource {
     } on DioError catch (error) {
       if (error.response?.statusCode == 400) {
         throw 'На этот день нчего нет(';
+      }
+      throw error.response?.data;
+    }
+  }
+  Future<VersionModel> getVersion() async {
+    try {
+      final Response response = await _dio.get('/archive/version');
+      return VersionModel.fromJson(response.data);
+    } on DioError catch (error) {
+      if (error.response?.statusCode == 400) {
+        throw 'Нет обновления';
       }
       throw error.response?.data;
     }
